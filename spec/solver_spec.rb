c81c1c427580bf57a6e89851e3e66d4077a5fec5
anbehindY <yanminthwin@gmail.com>
2023-07-06 06:13:52 +0630

    Add tests for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,18 @@
 require_relative '../solver'
 
-describe Dog do
-  
+describe Solver do
+  context 'when given a postive number or zero #factorial' do
+    it 'should return factorial of the given number' do
+      solver = Solver.new
+      expect(solver.factorial(5)).to eq(120)
+      expect(solver.factorial(0)).to eq(1)
+    end
+  end
+
+  context 'when given a negative number #factorial' do
+    it 'should raise an error'do
+      solver = Solver.new
+      expect{solver.factorial(-1)}.to raise_error("Negative number")
+    end
+  end
 end
