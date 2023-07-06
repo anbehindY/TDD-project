2e1acd096a1a20ab4992a0dd98c567a49789ed12
anbehindY <yanminthwin@gmail.com>
2023-07-06 08:12:50 +0630

    Add test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,18 +1,30 @@
 require_relative '../solver'
 
 describe Solver do
-  context 'when given a postive number or zero #factorial' do
-    it 'should return factorial of the given number' do
-      solver = Solver.new
-      expect(solver.factorial(5)).to eq(120)
-      expect(solver.factorial(0)).to eq(1)
+  describe '#factorial' do
+    context 'when given a postive number or zero' do
+      it 'should return factorial of the given number' do
+        solver = Solver.new
+        expect(solver.factorial(5)).to eq(120)
+        expect(solver.factorial(0)).to eq(1)
+      end
+    end
+
+    context 'when given a negative number ' do
+      it 'should raise an error'do
+        solver = Solver.new
+        expect{solver.factorial(-1)}.to raise_error("Negative number")
+      end
     end
   end
 
-  context 'when given a negative number #factorial' do
-    it 'should raise an error'do
-      solver = Solver.new
-      expect{solver.factorial(-1)}.to raise_error("Negative number")
+  describe '#reverse' do
+    context 'when a string is given' do
+      it 'should return the reversed string' do
+        solver = Solver.new
+        reversed_string = solver.reverse('hello')
+        expect(reversed_string.to eq('olleh'))
+      end
     end
   end
 end
